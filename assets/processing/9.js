c5c36ef80cbcc46b6dc92bd99ed1affa3ad6c155
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-02-10 16:40:02 +0900

    Update 9

--- assets/processing/9.js
+++ assets/processing/9.js
@@ -1,41 +1,12 @@
-float xstart, xnoise, ystart, ynoise;
+import processing.opengl.*;
 
-void setup () {
-  size(800, 450);
-  smooth();
-  background(0);
-  frameRate(24);
-  xstart = random(10);
-  ystart = random(10);
+void setup() {
+  size(500, 300, OPENGL);
+  sphereDetail(40);
 }
 
-void draw () {
-  background(0);
-  xstart += 0.01;
-  ystart += 0.01;
-
-  xnoise = xstart;
-  ynoise = ystart;
-
-  for (int y = 0; y <= height; y += 5) {
-    ynoise += 0.1;
-    xnoise = xstart;
-    for (int x = 0; x <= width; x += 5) {
-      xnoise += 0.1;
-      drawPoint(x, y, noise(xnoise, ynoise));
-    }
-  }
-}
-
-void drawPoint(float x, float y, float noiseFactor) {
-  pushMatrix();
-  translate(x, y);
-  rotate(noiseFactor * radians(540));
-  noStroke();
-  float edgeSize = noiseFactor * 35;
-  float grey = 150 + (noiseFactor * 120);
-  float alph = 150 + (noiseFactor * 120);
-  fill(grey, alph);
-  ellipse(0, 0, edgeSize, edgeSize / 2);
-  popMatrix();
+void draw() {
+  background(255);
+  translate(width / 2, height / 2, 0);
+  sphere(100);
 }
